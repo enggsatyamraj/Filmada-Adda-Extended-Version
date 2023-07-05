67b641da4c5174f7935037dd44052c0b326d473a
Satyaaam <sattu62025@gmail.com>
2023-07-05 16:41:18 +0530

    made it responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,16 +8,19 @@ const NavBar = () => {
   return (
     <div className='w-full px-4 py-4 transparent'>
         <div className='max-w-[1280px] flex items-center justify-between'>
-            <div className='flex gap-2'>
-                <BsFillCameraReelsFill size={30} className='text-[#fff]'/>
-                <span className='font-bold text-3xl text-[#fff]'>Filmada Adda.</span>
+            <div className='flex gap-2 items-center'>
+                <div>
+                <BsFillCameraReelsFill className='text-[#fff]  text-xl md:text-2xl'/>
+                </div>
+                <span className='font-bold text-[1.25rem] md:text-3xl text-[#fff] mt-[5px]'>Filmada Adda.</span>
             </div>
-            <div className='flex gap-5 items-center justify-center'>
+            <div className='flex md:gap-5 gap-3 items-center justify-center'>
                 <Link to="/signup">
-                    <button className='text-white bg-[#e50914] border-none px-3 py-1 rounded-lg font-semibold'>Sign Up</button>
+                    <button className='text-white bg-[#e50914] border-none px-3 py-1 text-[0.75rem] md:text-[1rem] rounded-lg font-semibold'>Sign Up</button>
                 </Link>
                 <Link to="/newuser">
-                    <button className='font-semibold border-White border-[2px] px-3 py-1 rounded-lg text-white'>New User</button>
+                    <button className='font-semibold border-White border-[2px] px-3 py-1 
+                    md:text-[1rem] text-[0.75rem] rounded-lg text-white'>New User</button>
                 </Link>
             </div>
         </div>
