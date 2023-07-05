a968018b3381ce06716edb1b8d42697c1a94ea44
Satyaaam <sattu62025@gmail.com>
2023-07-05 20:09:29 +0530

    added extra things

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -39,6 +39,65 @@ const HomePage = () => {
           </div>
         </div>
       </div>
+      <div className='w-full h-[10px] bg-gray-700'></div>
+      <div className='bg-black w-[100%] md:h-[100vh] h-fit max-w-[1280px]'>
+        <div className='text-white flex flex-col md:flex-row items-center justify-center w-[100%] md:h-[100%] h-fit px-6 py-[40px]'>
+          <div className='flex flex-col items-center justify-center flex-1 relative'>
+            <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" className='md:w-[80%] w-[90%]' alt="" />
+            <div className='flex items-center justify-between border-white border-[1px]
+            min-w-[250px] md:w-[40%] w-[50%] py-2 px-[10px] rounded'>
+              <div className='flex items-center justify-center gap-2'>
+                <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png" className='w-[40px]' alt="" />
+                <div>
+                  <h1 className='font-bold text-[1rem]'>Stranger Things</h1>
+                  <h3 className='text-[0.75rem]'>Downloading...</h3>
+                </div>
+              </div>
+              <div className='flex items-center justify-between'>
+                
+                <div>
+                  <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/download-icon.gif" alt="" className='w-[40px]'/>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className='flex-1 mt-[40px] md:mt-0'>
+            <h1 className='text-center md:text-left font-bold text-4xl mb-[20px] md:text-5xl'><span className='text-[#e50914]'>Download</span> your shows to watch offline</h1>
+            <p className='text-center text-[1.2rem] md:text-left'>Save your favourites easily and always have something to watch.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='w-full h-[10px] bg-gray-700'></div>
+      <div className='bg-black w-[100%] md:h-[100vh] h-fit max-w-[1280px]'>
+        <div className='text-white flex flex-col md:flex-row items-center justify-center w-[100%] md:h-[100%] h-fit px-6 py-[40px]'>
+          <div className='flex-1'>
+            <h1 className='text-center md:text-left font-bold text-4xl mb-[20px] md:text-5xl'><span className='text-[#e50914]'>Watch </span>everywhere</h1>
+            <p className='text-center text-[1.2rem] md:text-left'>
+              Stream unlimited movies and TV shows on your <span className='text-[#e50914]'>Phone</span>, tablet, laptop, and TV.
+            </p>
+          </div>
+          <div className='flex items-center justify-center flex-1'>
+            <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png" className='md:w-[80%] w-[90%]' alt="" />
+          </div>
+        </div>
+      </div>
+      <div className='w-full h-[10px] bg-gray-700'></div>
+      <div className='bg-black w-[100%] md:h-[100vh] h-fit max-w-[1280px]'>
+        <div className='text-white flex flex-col md:flex-row items-center justify-center w-[100%] md:h-[100%] h-fit px-6 py-[40px]'>
+          <div className='flex-1'>
+            <h1 className='text-center md:text-left font-bold text-4xl mb-[20px] md:text-5xl'>Create Profile for<span className='text-[#e50914]'> Kids </span></h1>
+            <p className='text-center text-[1.2rem] md:text-left md:w-[80%]'>
+            Send children on adventures with their favourite characters in a space made just for them—free with your membership.
+            </p>
+          </div>
+          <div className='flex items-center justify-center flex-1'>
+            <img src="https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVr8nYuAg0xDpXDv0VI9HUoH7r2aGp4TKRCsKNQrMwxzTtr-NlwOHeS8bCI2oeZddmu3nMYr3j9MjYhHyjBASb1FaOGYZNYvPBCL.png?r=54d" className='md:w-[80%] w-[90%]' alt=""/>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
