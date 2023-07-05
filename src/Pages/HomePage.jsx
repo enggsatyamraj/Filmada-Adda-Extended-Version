f72ae890dd4219c5b9bb0605056f9570b22549da
Satyaaam <sattu62025@gmail.com>
2023-07-05 18:43:17 +0530

    added more information

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,7 +5,7 @@ import {MdKeyboardArrowRight} from 'react-icons/md'
 const HomePage = () => {
   return (
     <div>
-      <div className="w-full object-cover bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/d282a426-b01a-424c-9b83-2c2445e4b61a/f7eb3bc2-2867-4c7e-94f8-e62ec11175cd/IN-en-20230626-popsignuptwoweeks-perspective_alpha_website_small.jpg')]">
+      <div className="w-full max-w-[1280px] object-cover bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/d282a426-b01a-424c-9b83-2c2445e4b61a/f7eb3bc2-2867-4c7e-94f8-e62ec11175cd/IN-en-20230626-popsignuptwoweeks-perspective_alpha_website_small.jpg')]">
         <div className='w-[100%] h-[100%]  bg-gradient-to-b from-[#232323] via-transparent to-[#232323]'>
           <NavBar/>
           <div className='w-100%  md:h-[100vh] px-1 md:px-2 pt-[60px] md:pt-0 pb-[60px] md:pb-0 h-fit  flex items-center justify-center flex-col'>
@@ -27,6 +27,18 @@ const HomePage = () => {
         </div>
       </div>
       <div className='w-full h-[10px] bg-gray-700'></div>
+      <div className='bg-black w-[100%] h-[100vh] max-w-[1280px]'>
+        <div className='text-white flex flex-col md:flex-row items-center justify-center w-[100%] h-[100%] px-6'>
+          <div className='flex-1'>
+            <h1 className='text-center md:text-left font-bold text-4xl mb-[20px] md:text-5xl'>Enjoy on your <span className='text-[#e50914]'>TV</span></h1>
+            <p className='text-center text-[1.2rem] md:text-left'>Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple Tv, Blu-ray players and more.
+            </p>
+          </div>
+          <div className='flex items-center justify-center flex-1'>
+            <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" className='md:w-[80%] w-[90%]' alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
